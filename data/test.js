6af3cb18ebe5dcef73cbc0ff0e0382691a01e14c
Michael Carr <xyeres@me.com>
2022-11-18 11:37:54 -0800

    feat: add leaflet

---
+++ data/test.js
@@ -0,0 +1,106 @@
+var data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        name: "Public Market"
+      },
+      "geometry": {
+        "coordinates": [
+          -122.33465793049128,
+          47.609473394238364
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -122.35084419297519,
+          47.616243008527874
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -122.33419939614353,
+          47.6178193682465
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -122.32406578705344,
+          47.61185367815082
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -122.3494936604639,
+          47.62897016169501
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -122.34054861739351,
+          47.637839417714275
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -122.32580194108812,
+          47.64315499714431
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -122.3124052480656,
+          47.62692513430349
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -122.31916567945063,
+          47.61559542547312
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
